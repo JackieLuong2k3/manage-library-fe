09700f385cc6ee29c2e1e79e11106b415f7e1778
QuynhLD <luongquynhduc4@gmail.com>
2025-08-13 18:01:03 +0700

    baseurlbaseurl

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -1,48 +1,50 @@
+const BASE_URL = "https://manage-library-be.onrender.com/api"
+
 export class Endpoints {
   static readonly Auth = {
-    REGISTER: "auth/register",
-    LOGIN: "auth/login",
-    LOGOUT: "auth/logout",
-    REFRESH: "auth/refresh",
-    CHANGE_PASSWORD_FIRST_TIME: "auth/change-password-first-time",
+    REGISTER: `${BASE_URL}/auth/register`,
+    LOGIN: `${BASE_URL}/auth/login`,
+    LOGOUT: `${BASE_URL}/auth/logout`,
+    REFRESH: `${BASE_URL}/auth/refresh`,
+    CHANGE_PASSWORD_FIRST_TIME: `${BASE_URL}/auth/change-password-first-time`,
   }
   static readonly Fine = {
-    GET_ALL: "fines",
-    GET_BY_ID: (id: string) => `fines/${id}`,
-    CREATE: "fines",
-    UPDATE: (id: string) => `fines/${id}`,
-    DELETE: (id: string) => `fines/${id}`,
-    PATCH: (id: string) => `fines/${id}/pay`,
-    GET_BY_USER: "fines/me",
+    GET_ALL: `${BASE_URL}/fines`,
+    GET_BY_ID: (id: string) => `${BASE_URL}/fines/${id}`,
+    CREATE: `${BASE_URL}/fines`,
+    UPDATE: (id: string) => `${BASE_URL}/fines/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/fines/${id}`,
+    PATCH: (id: string) => `${BASE_URL}/fines/${id}/pay`,
+    GET_BY_USER: `${BASE_URL}/fines/me`,
   }
   static readonly Review = {
-    CREATE: "reviews",      
-    GET_BY_USER_BY_BOOK_ID: (id: string) => `reviews/book/${id}/user`,  
-    GET_ALL: (id: string) => `reviews/book/${id}`,
-    UPDATE: (id: string) => `reviews/${id}`,
-    DELETE: (id: string) => `reviews/${id}`,
+    CREATE: `${BASE_URL}/reviews`,      
+    GET_BY_USER_BY_BOOK_ID: (id: string) => `${BASE_URL}/reviews/book/${id}/user`,  
+    GET_ALL: (id: string) => `${BASE_URL}/reviews/book/${id}`,
+    UPDATE: (id: string) => `${BASE_URL}/reviews/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/reviews/${id}`,
   }
   static readonly Users = {
-    GET_ALL: "users",
-    GET_ALLV2: "borrow-records/not-returned",
-    GET_BY_ID: (id: string) => `users/${id}`,
-    CREATE: "users",
-    UPDATE: (id: string) => `users/${id}`,
-    DELETE: (id: string) => `users/${id}`,
+    GET_ALL: `${BASE_URL}/users`,
+    GET_ALLV2: `${BASE_URL}/borrow-records/not-returned`,
+    GET_BY_ID: (id: string) => `${BASE_URL}/users/${id}`,
+    CREATE: `${BASE_URL}/users`,
+    UPDATE: (id: string) => `${BASE_URL}/users/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/users/${id}`,
   }
   static readonly Books = {
-    GET_ALL: "books",
-    GET_ALL_V2: "books/v2",
-    GET_BY_ID: (id: string) => `books/${id}`,
-    CREATE: "books",
-    UPDATE: (id: string) => `books/${id}`,
-    DELETE: (id: string) => `books/${id}`,
+    GET_ALL: `${BASE_URL}/books`,
+    GET_ALL_V2: `${BASE_URL}/books/v2`,
+    GET_BY_ID: (id: string) => `${BASE_URL}/books/${id}`,
+    CREATE: `${BASE_URL}/books`,
+    UPDATE: (id: string) => `${BASE_URL}/books/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/books/${id}`,
   }
   static readonly Categories = {
-    GET_ALL: 'categories',
-    GET_BY_ID: (id: string) => `categories/${id}`,
-    CREATE: 'categories',
-    UPDATE: (id: string) => `categories/${id}`,
-    DELETE: (id: string) => `categories/${id}`,
+    GET_ALL: `${BASE_URL}/categories`,
+    GET_BY_ID: (id: string) => `${BASE_URL}/categories/${id}`,
+    CREATE: `${BASE_URL}/categories`,
+    UPDATE: (id: string) => `${BASE_URL}/categories/${id}`,
+    DELETE: (id: string) => `${BASE_URL}/categories/${id}`,
   }
 }
